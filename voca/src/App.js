c456167dce95aa84688d8fe187a0b1f9f7d0a97f
Jeong-wonho <kallzzang@naver.com>
2022-05-13 01:15:35 +0900

    2022-05-12 study

--- voca/src/App.js
+++ voca/src/App.js
@@ -3,7 +3,10 @@ import Hello from "./component/Hello";
 function App() {
   return (
     <div className="App">
-      <Hello />
+      <h3>props: properties</h3>
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
   );
 }
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,42 +1,23 @@
-const Hello = function () {
-  function showName() {
-    console.log("Mike");
-  }
-  function showAge(age) {
-    console.log(age);
-  }
-  function showText(e) {
-    console.log(e.target.value);
-  }
+import { useState } from "react";
+import UserName from "./UserName";
+
+export default function Hello({ age }) {
+  const [name, setName] = useState("Mike");
+  const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
+
   return (
     <div>
-      <h1
-        style={{
-          color: "#f00",
-          borderRight: "12px solid #000",
-          marginBottom: "50px",
-          opacity: 0.5,
-        }}
-      >
-        Hello
-      </h1>
-      <button onClick={showName}>Show name</button>
+      <h2 id="name">
+        {name}({age}) : {msg}
+      </h2>
+      <UserName name={name} />
       <button
         onClick={() => {
-          showAge(10);
+          setName(name === "Mike" ? "Jane" : "Mike");
         }}
       >
-        Show age
+        Change
       </button>
-      <input
-        type="text"
-        onChange={(e) => {
-          const txt = e.target.value;
-          showText(txt);
-        }}
-      />
     </div>
   );
-};
-
-export default Hello;
+}
