f9819af2fad6d11d8a666f9021e5573d5cbaba27
Jeong-wonho <kallzzang@naver.com>
2022-05-27 00:48:46 +0900

    마이보카 디자인작업

---
+++ myvoca/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./component/Header";
+import WordList from "./component/WordButtonList";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <WordList />
+    </div>
+  );
+}
+
+export default App;
