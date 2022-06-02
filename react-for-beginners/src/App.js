6bbd3cf0fae28d07d46b72c8c539f18c8736eb01
Jeong-wonho <kallzzang@naver.com>
2022-06-02 15:05:43 +0900

    랜더링 2번 되는 문제 해결

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,35 +1,21 @@
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
 
+function Hello() {
   useEffect(() => {
-    console.log("I run only once.");
+    console.log("hi :)");
+    return () => console.log("bye :(");
   }, []);
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); //키워드가 변경할 때마다, 렌더링이 된다.
-
-  useEffect(() => {
-    console.log("I run when 'counter canges.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change");
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
-    <div className="App">
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
