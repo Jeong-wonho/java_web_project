d7440034b42bda684bd3df5af635c4bad005bfbb
Jeong-wonho <kallzzang@naver.com>
2022-06-03 21:49:36 +0900

    react-router-dom 6.3

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,18 +1,15 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/movie">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
     </Router>
   );
 }
