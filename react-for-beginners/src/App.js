add4b025551c22723de753fc47013a6e4d748401
Jeong-wonho <kallzzang@naver.com>
2022-06-03 09:06:59 +0900

    movie app 1

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,55 +1,42 @@
 import { useEffect, useState } from "react";
-function Moneychange({ coin }) {
-  const [dollar, setDollar] = useState(0);
-  const [count, setCount] = useState(0);
-  const onClick = (event) => {
-    setCount(Math.floor(dollar / coin.quotes.USD.price));
-    event.preventDefault();
-  };
-  const onChange = (event) => setDollar(event.target.value);
-  return (
-    <div>
-      <form>
-        <input
-          onChange={onChange}
-          type="text"
-          placeholder="달러를 입력하세요"
-        />
-        <button onClick={onClick}> 변환 </button>
-      </form>
-      <h3>you can buy {count} coin </h3>
-    </div>
-  );
-}
+
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coin, setCoin] = useState({});
-  const onClick = (event) => setCoin(coins[event.target.selectedIndex]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers?limit=1000")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      })
-      .then(() => setCoin(coins[0]));
+    getMovies();
   }, []);
 
   return (
     <div>
-      <h1>The Coins!({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select onChange={onClick}>
-        {coins.map((coin) => (
-          <option key={coin.id}>
-            {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
-          </option>
-        ))}
-      </select>
-      <hr />
-      {loading ? null : <Moneychange coin={coin} />}
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
