db2bcdab1191991cd2212f14ad614b8c835626fd
Jeong-wonho <kallzzang@naver.com>
2022-06-23 00:08:55 +0900

    grid css 어려웡..

--- myvoca/src/components/Voca.js
+++ myvoca/src/components/Voca.js
@@ -1,28 +1,21 @@
+import styles from "./Voca.module.scss";
 export default function Voca() {
   return (
-    <div class="main">
-      <ul>
-        <li>
-          <span>apple </span>
-          <span>사과</span>
-        </li>
-        <li>
-          <span>banana </span>
-          <span>바나나</span>
-        </li>
-        <li>
-          <span>watermelon </span>
-          <span>수박</span>
-        </li>
-        <li>
-          <span>melon </span>
-          <span>멜론</span>
-        </li>
-        <li>
-          <span>strawberry </span>
-          <span>딸기</span>
-        </li>
-      </ul>
+    <div className={styles.main}>
+      <table>
+        <thead>
+          <tr>
+            <td>English</td>
+            <td>한글</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Apple</td>
+            <td>사과</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
